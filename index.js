0aa942d250e8ff5871e3c9eec88dc8d7025a37a9
ismailJihad29 <ismailhossenjihad234@gmail.com>
2023-07-26 23:26:12 +0600

    get class data by id for update

--- index.js
+++ index.js
@@ -136,6 +136,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/class/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await classCollection.findOne(query);
+      res.send(result);
+    });
+
 
     app.get("/carts", verifyJWT, async (req, res) => {
       const email = req.query.email;
