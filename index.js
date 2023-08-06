11abba4e1b4ccfa8a1e0cd939732305c3ecd7bd2
ismailJihad29 <ismailhossenjihad234@gmail.com>
2023-08-06 19:52:58 +0600

    all done

--- index.js
+++ index.js
@@ -57,33 +57,12 @@ async function run() {
     const enrollClassCollection = client.db("rhythmDb").collection("carts");
     const paymentCollection = client.db("rhythmDb").collection("payments");
     const userCollection = client.db("rhythmDb").collection("users");
+    const reviewsCollection = client.db("rhythmDb").collection("reviews");
 
    
-    app.patch("/users/admin/:id", async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: new ObjectId(id) };
-      const updateDoc = {
-        $set: {
-          role: "admin",
-        },
-      };
-      const result = await userCollection.updateOne(filter, updateDoc);
-      res.send(result);
-    });
-    
-    app.patch("/users/instructor/:id", async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: new ObjectId(id) };
-      const updateDoc = {
-        $set: {
-          role: "instructor",
-        },
-      };
-      const result = await userCollection.updateOne(filter, updateDoc);
-      res.send(result);
-    });
+   
 
-// verify admin  
+// verify admin   and make instructor or admin  && other api 
     const verifyAdmin = async(req, res, next) => {
       const email = req.decoded.email
       const query = { email: email }
@@ -94,8 +73,19 @@ async function run() {
       next()
     }
 
-    // get functionality
-    // isAdmin
+   
+    app.get("/users", verifyJWT,verifyAdmin, async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/instructor", async (req, res) => {
+      const query = { role: "instructor"  }
+      const result = await userCollection.find(query).toArray()
+        res.send(result);
+    })
+
+
     app.get("/users/admin/:email", verifyJWT, async (req, res) => {
       const email = req.params.email;
 
@@ -125,19 +115,83 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
 
+    app.post("/users", async (req, res) => {
+      const user = req.body;
 
-    app.get("/users", verifyJWT,verifyAdmin, async (req, res) => {
-      const result = await userCollection.find().toArray();
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "User already exists" });
+      }
+      const result = await userCollection.insertOne(user);
       res.send(result);
     });
 
-    app.get("/instructor", async (req, res) => {
-      const query = { role: "instructor"  }
-      const result = await userCollection.find(query).toArray()
-        res.send(result);
+    app.post("/checkuser-role", async (req, res) => {
+      const email = req.body?.email;
+      if (email) {
+        const query = { email: email };
+        const result = await userCollection.findOne(query);
+        if (result) {
+          res.send({ role: result?.role });
+        }
+      } else {
+        res.send({ role: null });
+      }
+    });
+    
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+    
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "instructor",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    app.delete("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+
+
+    // reviews
+
+    app.get("/reviews", async (req, res) => { 
+      const result = await reviewsCollection.find().toArray()
+      res.send(result);
     })
 
+/**--------------------------------------------------------------------
+ *  class api 
+ */
 
     app.get("/class",  async (req, res) => {
       const result = await classCollection.find().toArray();
@@ -151,6 +205,94 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/class", async(req, res) => { 
+      const classes = req.body
+      console.log('new toy', classes);
+      const result = await classCollection.insertOne(classes);
+      res.send(result)
+    })
+
+    app.post("/class/:id", async (req, res) => { 
+      const id = req.params.id;
+      const updateStatus = req.body.status
+      const filter = { _id: new ObjectId(id) }
+      const updetedStatus = {
+        $set: {
+          status: updateStatus
+        }
+      }
+      const result = await classCollection.updateOne(filter, updetedStatus);
+      res.send(result)
+    }) 
+
+
+    app.put("/update-seat-enrollment/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const { available_seat, enrolled_student } = req.body;
+      console.log({ id, enrolled_student, available_seat });
+      const filter = {
+        _id: new ObjectId(id),
+      };
+      const updateClassInfo = {
+        $set: {
+          available_seat: available_seat - 1,
+          enrolled_student: enrolled_student + 1,
+        },
+      };
+      const options = { upsert: false };
+      const updatedResult = await classCollection.updateOne(
+        filter,
+        updateClassInfo,
+        options
+      );
+
+      res.send({ updatedResult });
+    });
+
+
+    app.put("/feedback/:id", async (req, res) => { 
+      const id = req.params.id;
+      const updateFeedback = req.body.feedback
+      const filter = { _id: new ObjectId(id) }
+      const feedback = {
+        $set: {
+          feedback: updateFeedback
+        }
+      }
+      const result = await classCollection.updateOne(filter, feedback);
+      res.send(result)
+    }) 
+
+    app.patch("/class/:id", async (req, res) => { 
+      const id = req.params.id;
+      const updatedClass = req.body
+      const filter = { _id: new ObjectId(id) }
+      const newClass = {
+        $set: {
+          class_name: updatedClass. class_name,
+          class_image: updatedClass.class_image,
+          price: updatedClass.price,
+          available_seat: updatedClass.available_seat,
+        }
+      }
+      const result = await classCollection.updateOne(filter, newClass, )
+      res.send(result)
+    })
+
+    app.delete("/class/:id", async (req, res) => { 
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await classCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    // ----------------------------------------------------------------------------
+
+
+
+  /**-----------------------------------------------------------------------------
+   *  Carts api 
+   */
 
     app.get("/carts", verifyJWT, async (req, res) => {
       const email = req.query.email;
@@ -176,6 +318,50 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/carts", async (req, res) => {
+      const item = req.body;
+      const findAddedClass = await enrollClassCollection.findOne({
+        class_id: item?.class_id,
+        email: item?.email
+      })
+      if (!findAddedClass) {
+        const result = await enrollClassCollection.insertOne(item)
+        res.send(result)
+      }
+      else {
+        res.send("already added")
+      }
+    });
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await enrollClassCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // --------------------------------------------------------------------
+
+
+
+    /**-----------------------------------------------------------------
+     * payments and enrolled api 
+     */
+    app.get("/enrolled", verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res
+          .status(403)
+          .send({ error: true, message: "Forbidden access" });
+      }
+      const query = { email: email };
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
+    });
 
     app.post("/create-payment-intent", verifyJWT, async (req, res) => { 
       const {price} = req.body
@@ -190,137 +376,22 @@ async function run() {
       });
     })
 
-    app.post("/payments", async (req, res) => { 
+    app.post("/payments", verifyJWT, async (req, res) => { 
       const payment = req.body 
       const result = await paymentCollection.insertOne(payment)
       res.send(result);
     })
 
-    /**
-     * ----------------------------------------------------------------
-     * Post Methods
-     * ----------------------------------------------------------------
-     */
-    app.post("/jwt", (req, res) => {
-      const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
-        expiresIn: "1h",
-      });
-      res.send({ token });
-    });
 
+    // ------------------------------------------------------------------
 
-    app.post("/class", async(req, res) => { 
-      const classes = req.body
-      console.log('new toy', classes);
-      const result = await classCollection.insertOne(classes);
-      res.send(result)
-    })
-    app.post("/carts", async (req, res) => {
-      const item = req.body;
-      const result = await enrollClassCollection.insertOne(item);
-      res.send(result);
-    });
-
-
-    app.post("/users", async (req, res) => {
-      const user = req.body;
-
-      const query = { email: user.email };
-      const existingUser = await userCollection.findOne(query);
-      if (existingUser) {
-        return res.send({ message: "User already exists" });
-      }
-      const result = await userCollection.insertOne(user);
-      res.send(result);
-    });
-
-    app.post("/class/:id", async (req, res) => { 
-      const id = req.params.id;
-      const updateStatus = req.body.status
-      const filter = { _id: new ObjectId(id) }
-      const updetedStatus = {
-        $set: {
-          status: updateStatus
-        }
-      }
-      const result = await classCollection.updateOne(filter, updetedStatus);
-      res.send(result)
-    }) 
-
-    app.put("/feedback/:id", async (req, res) => { 
-      const id = req.params.id;
-      const updateFeedback = req.body.feedback
-      const filter = { _id: new ObjectId(id) }
-      const feedback = {
-        $set: {
-          feedback: updateFeedback
-        }
-      }
-      const result = await classCollection.updateOne(filter, feedback);
-      res.send(result)
-    }) 
-
-
-    app.patch("/class/:id", async (req, res) => { 
-      const id = req.params.id;
-      const updatedClass = req.body
-      const filter = { _id: new ObjectId(id) }
-      const newClass = {
-        $set: {
-          class_name: updatedClass. class_name,
-          class_image: updatedClass.class_image,
-          price: updatedClass.price,
-          available_seat: updatedClass.available_seat,
-        }
-      }
-      const result = await classCollection.updateOne(filter, newClass, )
-      res.send(result)
-    })
-
-    // check users role 
-
-    app.post("/checkuser-role", async (req, res) => {
-      const email = req.body?.email;
-      if (email) {
-        const query = { email: email };
-        const result = await userCollection.findOne(query);
-        if (result) {
-          res.send({ role: result?.role });
-        }
-      } else {
-        res.send({ role: null });
-      }
-    });
-
-    //  delete functionality
-    app.delete("/carts/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await enrollClassCollection.deleteOne(query);
-      res.send(result);
-    });
-
-    app.delete("/class/:id", async (req, res) => { 
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
-      const result = await classCollection.deleteOne(query)
-      res.send(result)
-    })
-
-    app.delete("/users/admin/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await userCollection.deleteOne(query);
-      res.send(result);
-    });
-    // Send a ping to confirm a successful connection
+   
+  
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
